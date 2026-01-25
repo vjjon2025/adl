15ba30c18584b6055d22f524453c199631180594
vjjon2025 <jvjonnalagadda@gmail.com>
2026-01-25 15:36:35 -0800

    Half precision changes

--- hw/hw1/homework/half_precision.py
+++ hw/hw1/homework/half_precision.py
@@ -17,14 +17,23 @@ class HalfLinear(torch.nn.Linear):
         Feel free to use the torch.nn.Linear class as a parent class (it makes load_state_dict easier, names match).
         Feel free to set self.requires_grad_ to False, we will not backpropagate through this layer.
         """
-        # TODO: Implement me
-        raise NotImplementedError()
+        super().__init__(in_features, out_features, bias)
+        self.half() #same as self.to(torch.float16)
+        #self.bfloat16() #same as self.to(torch.bfloat16)
+        self.requires_grad_(False) #we will not backpropagate through this layer
+        # DONE: Implement me
+        #raise NotImplementedError()
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         # Hint: Use the .to method to cast a tensor to a different dtype (i.e. torch.float16 or x.dtype)
         # The input and output should be of x.dtype = torch.float32
-        # TODO: Implement me
-        raise NotImplementedError()
+        # DONE: Implement me
+        # Only on GPU (fp16 Linear is not for CPU)
+        if not x.is_cuda:
+            raise RuntimeError("HalfLinear requires CUDA")
+        y = super().forward(x.to(torch.float16))
+        return y.to(x.dtype)
+        #raise NotImplementedError()
 
 
 class HalfBigNet(torch.nn.Module):
@@ -36,16 +45,36 @@ class HalfBigNet(torch.nn.Module):
     class Block(torch.nn.Module):
         def __init__(self, channels: int):
             super().__init__()
-            # TODO: Implement me (feel free to copy and reuse code from bignet.py)
-            raise NotImplementedError()
+            #Done: feel free to copy and reuse code from bignet.py)
+            self.model = torch.nn.Sequential(
+                HalfLinear(channels, channels),
+                torch.nn.ReLU(),
+                HalfLinear(channels, channels),
+                torch.nn.ReLU(),
+                HalfLinear(channels, channels),
+            )
+            #raise NotImplementedError()
 
         def forward(self, x: torch.Tensor):
             return self.model(x) + x
 
     def __init__(self):
         super().__init__()
-        # TODO: Implement me (feel free to copy and reuse code from bignet.py)
-        raise NotImplementedError()
+        # Done: Implement me (feel free to copy and reuse code from bignet.py)
+        self.model = torch.nn.Sequential(
+            self.Block(BIGNET_DIM),
+            LayerNorm(BIGNET_DIM),
+            self.Block(BIGNET_DIM),
+            LayerNorm(BIGNET_DIM),
+            self.Block(BIGNET_DIM),
+            LayerNorm(BIGNET_DIM),
+            self.Block(BIGNET_DIM),
+            LayerNorm(BIGNET_DIM),
+            self.Block(BIGNET_DIM),
+            LayerNorm(BIGNET_DIM),
+            self.Block(BIGNET_DIM),
+        )
+        #raise NotImplementedError()
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return self.model(x)
